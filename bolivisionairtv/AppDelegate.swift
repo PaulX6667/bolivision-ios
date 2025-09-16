9d5e71e1b53a159790354473b577dfab8b43dd2e
PaulX6667 <73670021+PaulX6667@users.noreply.github.com>
2025-09-16 08:19:02 -0400

    Update AppDelegate.swift

--- bolivisionairtv/AppDelegate.swift
+++ bolivisionairtv/AppDelegate.swift
@@ -1,25 +1,50 @@
-@UIApplicationMain
+import UIKit
+import GoogleMobileAds
+import FBSDKCoreKit
+
+//@UIApplicationMain
+@main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    var window: UIWindow?
+    //var window: UIWindow?
 
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
         // FirebaseApp.configure() // comentado
+        
         ApplicationDelegate.shared.application(application, didFinishLaunchingWithOptions: launchOptions)
         GADMobileAds.sharedInstance().start(completionHandler: nil)
 
-        window = UIWindow(frame: UIScreen.main.bounds)
-        let nav = UINavigationController(rootViewController: HomeViewController())
-        window?.rootViewController = nav
-        window?.makeKeyAndVisible()
+        //window = UIWindow(frame: UIScreen.main.bounds)
+        //let nav = UINavigationController(rootViewController: HomeViewController())
+        //window?.rootViewController = nav
+        //window?.makeKeyAndVisible()
 
         return true
     }
 
-    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
-        let handled = ApplicationDelegate.shared.application(app, open: url, options: options)
-        return handled
+    // MARK: UISceneSession Lifecycle
+    func application(
+        _ application: UIApplication,
+        configurationForConnecting connectingSceneSession: UISceneSession,
+        options: UIScene.ConnectionOptions
+    ) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
+
+    func application(
+        _ application: UIApplication,
+        didDiscardSceneSessions sceneSessions: Set<UISceneSession>
+    ) {}
+
+    // Facebook SDK handler
+    func application(
+        _ app: UIApplication,
+        open url: URL,
+        options: [UIApplication.OpenURLOptionsKey : Any] = [:]
+    ) -> Bool {
+        return ApplicationDelegate.shared.application(app, open: url, options: options)
+    }
+
 }
